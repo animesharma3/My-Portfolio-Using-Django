b0db24724f346384f4ff996c039aa2d00820bf84
Animesh Sharma <animesharma3@gmail.com>
2021-06-11 23:50:38 +0530

    Postgre DB connection using ElephantSQL

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -99,8 +99,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'lvyumola',
+        'USER': 'lvyumola',
+        'PASSWORD': 'ZVg3vPXhdsTvCcFp8VgFhk8mEsxZGoUF',
+        'HOST': 'batyr.db.elephantsql.com',
+        'PORT': '5432'
     }
 }
 
